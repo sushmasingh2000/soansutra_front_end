caed3442ba9003876489d8a8fb67ade159714894
krishnasingh16 <krishnasingh12sep@gmail.com>
2025-10-31 17:00:44 +0000

    hg

--- src/components/pages/POS/sub/Footer.js
+++ src/components/pages/POS/sub/Footer.js
@@ -1,6 +1,6 @@
 export default function POSFooter() {
   return (
-    <div className="grid grid-cols-6 gap-2 p-3 bg-gray-50 border-t mt-2 text-sm">
+    <div className="grid grid-cols-6 gap-2 p-3 bg-gray-50 border-t text-sm mt-72">
       <div><b>Quantity</b><br />0</div>
       <div><b>Sale Price</b><br />0.00</div>
       <div><b>Tax Amount</b><br />0.00</div>
